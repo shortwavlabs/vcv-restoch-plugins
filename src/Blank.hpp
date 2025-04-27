e11c81b309713feb1d39f3ecc5f6d361f203f439
Stephane P. Pericat <contact@pericat.net>
2025-04-27 11:32:25 -0500

    feat: blank panel

--- src/Blank.hpp
+++ src/Blank.hpp
@@ -36,7 +36,7 @@ struct BlankWidget : ModuleWidget
   BlankWidget(Blank *module)
   {
     setModule(module);
-    setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/3HP.svg")));
+    setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Blank.svg")));
 
     addChild(createWidget<ScrewSilver>(Vec(0, 0)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 1 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
