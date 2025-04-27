19d9b7129024c006f287d5bac8c42c374ab41ee2
Stephane P. Pericat <contact@pericat.net>
2025-04-27 13:23:58 -0500

    chore: update restitcher panel

--- src/ReStitcher.hpp
+++ src/ReStitcher.hpp
@@ -194,7 +194,7 @@ struct ReStitcherWidget : ModuleWidget
   ReStitcherWidget(ReStitcher *module)
   {
     setModule(module);
-    setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/24HP.svg")));
+    setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/23HP_Blank.svg")));
 
     addChild(createWidget<ScrewSilver>(Vec(0, 0)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 1 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
